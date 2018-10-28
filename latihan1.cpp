2d7f7a76cab8081f0c06b3dbcdc71a5e7bca1817
ihwaljanuar <ihwaljan01@gmail.com>
2018-10-28 09:48:31 +0700

    menambahkan file baru

---
+++ latihan1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int p, l , luas;
+
+    cout<< "masukan panjang: " ;
+    cin >> p;
+    cout<< "msukan lebar: " ;
+    cin>> l;
+
+    luas = p*l;
+
+    cout <<"luas persegi panjang adalah: "<< luas << endl ;
+
+
+}
